c3fdbc40ae84be777027a62085654d2d44bae44d
Chris Patti <feoh@feoh.org>
2024-06-10 23:26:41 -0400

    Add script

--- fedi_archive_search/fedi_archive_search.py
+++ fedi_archive_search/fedi_archive_search.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.title("Fediverse Archive Searcher")
 
-from_date = st.date_input("From date")
-to_date = st.date_input("To date")
 
-search_term = st.text_input("Search term")
+def main():
+    from_date = st.date_input("From date")
+    to_date = st.date_input("To date")
+
+    search_term = st.text_input("Search term")
