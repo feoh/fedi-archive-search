62bd350596fee04a1264d6b136e943a3966d982f
Chris Patti <feoh@feoh.org>
2024-06-12 20:21:25 -0400

    Fixed select. The UI is not displaying data though

--- fedi_archive_search.py
+++ fedi_archive_search.py
@@ -9,8 +9,6 @@ to_date = st.date_input("To date")
 
 search_term = st.text_input("Search term")
 
-j = duckdb.read_json('oldbytes.space.user.feoh.json', maximum_object_size=640000000);
+statuses=duckdb.sql("select statuses FROM read_json_auto('oldbytes.space.user.feoh.json', maximum_object_size=160000000)")
 
-
-statuses = duckdb.sql("select statuses from j")
-print(statuses)
+st.table(statuses)
