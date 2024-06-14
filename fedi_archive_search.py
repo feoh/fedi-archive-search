716e2adebb424800782910f02d0edf5c25935d26
Chris Patti <feoh@feoh.org>
2024-06-13 21:45:57 -0400

    Add filter. Not working

--- fedi_archive_search.py
+++ fedi_archive_search.py
@@ -9,7 +9,11 @@ to_date = st.date_input("To date")
 
 search_term = st.text_input("Search term")
 
-statuses=duckdb.sql("select statuses FROM read_json_auto('oldbytes.space.user.feoh.json', maximum_object_size=160000000)")
+statuses = duckdb.sql(
+    "select * FROM read_json_auto(oldbytes.space.user.feoh.json, maximum_object_size=160000000)"
+)
+
+# statuses.filter('content LIKE search_term')
 
 
 st.write(statuses)
